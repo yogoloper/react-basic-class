813404bc7e81807d07d47485ea0b2f56c6c5dc13
yogoloper <jguddnjs@gmail.com>
2024-02-23 19:59:24 +0900

    이벤트 내 state 변경

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ class App extends Component {
         {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
         <header>
           <h1><a href='/' onClick={function(e) {
-            alert('hello~');
-            e.preventDefault(); 
-          }}>{this.state.subject.title}</a></h1>
+            e.preventDefault();
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}
         </header>
         <TOC data={this.state.contents}></TOC>
