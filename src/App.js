083931f8b74e662b955993fbd76b4fbe14694a0f
yogoloper <jguddnjs@gmail.com>
2024-02-23 18:56:46 +0900

    스테이트 적용

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ import Subject from './components/Subject';
 import Content from './components/Content';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: 'Web', sub: 'world wide web!' },
+      contents: [
+        {id: 1, title: 'HTML', desc: 'HTML is Hypertext Markup Language.'},
+        {id: 2, title: 'CSS', desc: 'CSS is ...'},
+        {id: 3, title: 'JavaSCript', desc: 'JavaSCript is ...'},
+      ],
+    }
+  }
+
   render() {
     return (
       <div className='App'>
-        <Subject title='Web' sub='world wide web!'></Subject>
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title='HTML' desc='HTML is Hypertext Markup Language.'></Content>
       </div>
     )
