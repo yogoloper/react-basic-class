66ba18c6db8967ebf46a67578531692fdc6ca8bf
yogoloper <jguddnjs@gmail.com>
2024-02-23 19:54:29 +0900

    이벤트 추가

--- src/App.js
+++ src/App.js
@@ -30,7 +30,14 @@ class App extends Component {
     }
     return (
       <div className='App'>
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        <header>
+          <h1><a href='/' onClick={function(e) {
+            alert('hello~');
+            e.preventDefault(); 
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
