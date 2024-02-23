fe862a3414bedc90a827cef297bb83db4d12fbbf
yogoloper <jguddnjs@gmail.com>
2024-02-23 11:32:27 +0900

    Subject 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import { Component } from 'react';
 
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        Hello, React!!
+        <Subject></Subject>
       </div>
     )
   }
