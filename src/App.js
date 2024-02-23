380dafad142ad252c4b99432ed96b7596c796d05
yogoloper <jguddnjs@gmail.com>
2024-02-23 21:59:40 +0900

    props로 이벤트 전달, setState로 상태 변경

--- src/App.js
+++ src/App.js
@@ -30,14 +30,14 @@ class App extends Component {
     }
     return (
       <div className='App'>
-        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
-        <header>
-          <h1><a href='/' onClick={function(e) {
-            e.preventDefault();
-            this.setState({ mode: 'welcome' });
-          }.bind(this)}>{this.state.subject.title}</a></h1>
-          {this.state.subject.sub}
-        </header>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({ mode:'welcome' });
+          }.bind(this)}>
+        </Subject>
+        
         <TOC data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
